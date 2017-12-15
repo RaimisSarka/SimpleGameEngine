4bffa7b0158c4f71af402966a2181c3d4d3c9bcc
RaimisSarka <raimis.sarka@gmail.com>
2017-12-15 05:29:28 +0200

    Bob changedto chibi, added buttons

--- app/src/main/java/com/raimissarka/simplegameengine/SimpleGameEngine.java
+++ app/src/main/java/com/raimissarka/simplegameengine/SimpleGameEngine.java
@@ -39,7 +39,7 @@ public class SimpleGameEngine extends Activity {
 
     // Notice we implement runnable so we have
     // A thread and can override the run method.
-    class GameView extends SurfaceView implements Runnable {
+    class GameView extends SurfaceView implements Runnable, SurfaceHolder.Callback{
 
         // This is our thread
         Thread gameThread = null;
@@ -65,15 +65,41 @@ public class SimpleGameEngine extends Activity {
 
         // Declare an object of type Bitmap
         Bitmap bitmapBob;
+        Bitmap cropedImage;
+        Bitmap buttonToRight;
+        Bitmap buttonToLeft;
 
         // Bob starts off not moving
         boolean isMoving = false;
 
+        //Set moving direction
+        boolean movingToRight = true;
+        boolean movingToLeft = false;
+
+        //if false - image collumns is counting from left to right, else form right to left
+        boolean stepImageDirection = false;
+
         // He can walk at 150 pixels per second
         float walkSpeedPerSecond = 150;
 
+        public int wholeImageWidth;
+        public int wholeImageHeight;
+
+        public int imageCropWidth;
+        public int imageCropHeight;
+
+        public int col;
+        public int row;
+
+        public int[] buttonToLeftArea = {0, 0, 0, 0};
+        public int[] buttonToRightArea = {0, 0, 0, 0};
+
         // He starts 10 pixels from the left
-        float bobXPosition = 10;
+        public final static int startPositionX  = 10;
+
+        float bobXPosition = startPositionX;
+        float nextImagePosition;
+        float nextCol;
 
         // When the we initialize (call new()) on gameView
         // This special constructor method runs
@@ -88,12 +114,54 @@ public class SimpleGameEngine extends Activity {
             paint = new Paint();
 
             // Load Bob from his .png file
-            bitmapBob = BitmapFactory.decodeResource(this.getResources(), R.drawable.bob);
+            bitmapBob = BitmapFactory.decodeResource(this.getResources(), R.drawable.chibi1);
+            //Load control buttons
+            buttonToLeft = BitmapFactory.decodeResource(this.getResources(), R.drawable.button_go_to_the_left);
+            buttonToRight = BitmapFactory.decodeResource(this.getResources(), R.drawable.button_go_to_right);
 
+            wholeImageWidth = bitmapBob.getWidth();
+            wholeImageHeight = bitmapBob.getHeight();
+
+            buttonToLeftArea [0] = 10;
+            buttonToLeftArea [1] = wholeImageHeight + 40;
+            buttonToLeftArea [2] = buttonToLeft.getWidth() + buttonToLeftArea[0];
+            buttonToLeftArea [3] = buttonToLeft.getHeight() + buttonToLeftArea[2];
+
+            buttonToRightArea [0] = 310;
+            buttonToRightArea [1] = wholeImageHeight + 40;
+            buttonToRightArea [2] = buttonToRight.getWidth() + buttonToRightArea[0];
+            buttonToRightArea [3] = buttonToRight.getHeight() + buttonToRightArea[2];
+
+            imageCropWidth = wholeImageWidth / 3;
+            imageCropHeight = wholeImageHeight / 4;
+            nextCol = (imageCropWidth + startPositionX) / 3;
+            nextImagePosition = nextCol;
+
+            row = 2;
+            col = 0;
+
+            cropedImage = Bitmap.createBitmap(bitmapBob, imageCropWidth * col,
+                    imageCropHeight * row, imageCropWidth, imageCropHeight);
             // Set our boolean to true - game on!
             playing = true;
 
         }
+
+        public Bitmap GameViewUpdate (int c, int r){
+            bitmapBob = BitmapFactory.decodeResource(this.getResources(), R.drawable.chibi1);
+
+            wholeImageWidth = bitmapBob.getWidth();
+            wholeImageHeight = bitmapBob.getHeight();
+
+            imageCropWidth = wholeImageWidth / 3;
+            imageCropHeight = wholeImageHeight / 4;
+
+            cropedImage = Bitmap.createBitmap(bitmapBob, imageCropWidth * col,
+                    imageCropHeight * row, imageCropWidth, imageCropHeight);
+
+            return cropedImage;
+        }
+
 
         @Override
         public void run() {
@@ -129,6 +197,22 @@ public class SimpleGameEngine extends Activity {
             // then move him to the right based on his target speed and the current fps.
             if(isMoving){
                 bobXPosition = bobXPosition + (walkSpeedPerSecond / fps);
+                if(bobXPosition > nextImagePosition ) {
+                    if (!stepImageDirection) {
+                        gameView.col++;
+                    } else {
+                        gameView.col--;
+                    }
+                    nextImagePosition = nextImagePosition + nextCol;
+                }
+                if (gameView.col>=2){
+                    //revert direction
+                    stepImageDirection = true;
+                } else if (gameView.col <=0) {
+                    stepImageDirection = false;
+                }
+
+                cropedImage = GameViewUpdate(col, row);
             }
 
         }
@@ -152,9 +236,17 @@ public class SimpleGameEngine extends Activity {
 
                 // Display the current fps on the screen
                 canvas.drawText("FPS:" + fps, 20, 40, paint);
+                canvas.drawText("row" + row, 20, 80, paint);
+                canvas.drawText("col" + col, 20, 120, paint);
+                canvas.drawText("imageCropWidth" + imageCropWidth, 120, 40, paint);
+                canvas.drawText("imageCropHeight" + imageCropHeight, 120, 80, paint);
+                canvas.drawText("imageWidth" + bitmapBob.getWidth(), 120, 120, paint);
+                canvas.drawText("bobXposition" + bobXPosition, 20, 160, paint);
 
                 // Draw bob at bobXPosition, 200 pixels
-                canvas.drawBitmap(bitmapBob, bobXPosition, 200, paint);
+                canvas.drawBitmap(gameView.cropedImage, bobXPosition, 200, paint);
+                canvas.drawBitmap(buttonToLeft, buttonToLeftArea[0], buttonToLeftArea[1], paint);
+                canvas.drawBitmap(buttonToRight, buttonToRightArea[0], buttonToRightArea[1], paint);
 
                 // Draw everything to the screen
                 ourHolder.unlockCanvasAndPost(canvas);
@@ -193,8 +285,12 @@ public class SimpleGameEngine extends Activity {
                 case MotionEvent.ACTION_DOWN:
 
                     // Set isMoving so Bob is moved in the update method
-                    isMoving = true;
-
+                    int x=  (int)motionEvent.getX();
+                    int y = (int)motionEvent.getY();
+                    if ( x > buttonToRightArea[0] && x < buttonToRightArea [2]
+                            && y > buttonToRightArea[1] && y < buttonToRightArea[3]) {
+                        isMoving = true;
+                    }
                     break;
 
                 // Player has removed finger from screen
@@ -208,6 +304,20 @@ public class SimpleGameEngine extends Activity {
             return true;
         }
 
+        @Override
+        public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+        }
     }
     // This is the end of our GameView inner class
 
